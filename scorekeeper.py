d13386c368a4261932ad6c77e36683c635a12e43
Pranj Patel <pranj1012@gmail.com>
2020-12-21 21:36:00 -0700

    Started main

--- scorekeeper.py
+++ scorekeeper.py
@@ -1,3 +1,23 @@
-import tkinter
-top = tkinter.Tk()
-top.mainloop()
+import tkinter as tk
+import classes
+import sys
+
+def EndGame():
+    sys.exit(1)
+
+def main():
+    status = "initial"
+    if status == "initial":
+        score = tk.Tk()
+        score.geometry("300x300")
+        start = tk.Button(score,text = "New Game")
+        start.place(x=0,y=0)
+        exit = tk.Button(score,text = "Exit",command = EndGame)
+        exit.place(x=65,y=0)
+        score.mainloop()
+    elif status == "game":
+        pass
+
+
+if __name__ == "__main__":
+    main()
