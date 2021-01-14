4b6ee71a7df14c2e069b455c7339f28125862b81
Pranj Patel <pranj1012@gmail.com>
2021-01-13 19:49:50 -0700

    Converted to Object Oriented Code

--- scorekeeper.py
+++ scorekeeper.py
@@ -1,72 +1,75 @@
-import tkinter as tk
 from tkinter import *
 import sys
 
-def EndGame():
-    sys.exit(1)
+class Scorekeeper(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Scorekeeper")
+        self.geometry("420x200")
 
-def Start():
-    player1.grid_forget()
-    player2.grid_forget()
-    player3.grid_forget()
-    player4.grid_forget()
+        self.start = Button(self,text = "New Game", height = 5, width = 20,
+                          command = self.Players, bg = "green")
+        self.exit = Button(self,text = "Exit",height = 5, width = 20,command =
+                         self.EndGame, bg = "red")
 
-    instruct.place_forget()
-    contin.place_forget()
+        self.start.place(x=58,y=50)
+        self.exit.place(x=208,y=50)
 
-def Players():
-    start.place_forget()
-    exit.place_forget()
-    score.geometry("420x200")
+    def EndGame(self):
+        sys.exit(1)
 
-    player1.grid(row = 0, column = 0, padx = 20, pady = 70)
-    player2.grid(row = 0, column = 1, padx = 20, pady = 70)
-    player3.grid(row = 0, column = 2, padx = 20, pady = 70)
-    player4.grid(row = 0, column = 3, padx = 20, pady = 70)
+    def Start(self):
+        self.player1.grid_forget()
+        self.player2.grid_forget()
+        self.player3.grid_forget()
+        self.player4.grid_forget()
 
-    p1entry.grid(row = 1, column = 0)
-    p1label.grid(row = 0, column = 0)
-    p2entry.grid(row = 1, column = 0)
-    p2label.grid(row = 0, column = 0)
-    p3entry.grid(row = 1, column = 0)
-    p3label.grid(row = 0, column = 0)
-    p4entry.grid(row = 1, column = 0)
-    p4label.grid(row = 0, column = 0)
+        self.instruct.place_forget()
+        self.contin.place_forget()
 
-    instruct.place(x=158,y=16)
-    contin.place(x=175,y=150)
+    def Players(self):
+
+        self.player1 = Frame(self)
+        self.p1entry = Entry(self.player1, width=10)
+        self.p1label = Label(self.player1, text="Player 1")
+
+        self.player2 = Frame(self)
+        self.p2entry = Entry(self.player2, width=10)
+        self.p2label = Label(self.player2, text="Player 2")
+
+        self.player3 = Frame(self)
+        self.p3entry = Entry(self.player3, width=10)
+        self.p3label = Label(self.player3, text="Player 3")
+
+        self.player4 = Frame(self)
+        self.p4entry = Entry(self.player4, width=10)
+        self.p4label = Label(self.player4, text="Player 4")
+
+        self.instruct = Label(self, text = "Enter your names")
+        self.contin = Button(self, text = "Continue", command = self.Start, bg = "green")
+
+        self.start.place_forget()
+        self.exit.place_forget()
+        self.geometry("420x200")
+
+        self.player1.grid(row = 0, column = 0, padx = 20, pady = 70)
+        self.player2.grid(row = 0, column = 1, padx = 20, pady = 70)
+        self.player3.grid(row = 0, column = 2, padx = 20, pady = 70)
+        self.player4.grid(row = 0, column = 3, padx = 20, pady = 70)
+
+        self.p1entry.grid(row = 1, column = 0)
+        self.p1label.grid(row = 0, column = 0)
+        self.p2entry.grid(row = 1, column = 0)
+        self.p2label.grid(row = 0, column = 0)
+        self.p3entry.grid(row = 1, column = 0)
+        self.p3label.grid(row = 0, column = 0)
+        self.p4entry.grid(row = 1, column = 0)
+        self.p4label.grid(row = 0, column = 0)
+
+        self.instruct.place(x=158,y=16)
+        self.contin.place(x=175,y=150)
 
 if __name__ == "__main__":
 
-    score = tk.Tk()
-    score.title("Scorekeeper")
-    score.geometry("420x200")
-
-    start = tk.Button(score,text = "New Game", height = 5, width = 20, command
-                       = Players, bg = "green")
-    exit = tk.Button(score,text = "Exit",height = 5, width = 20,command =
-                     EndGame, bg = "red")
-
-    start.place(x=58,y=50)
-    exit.place(x=208,y=50)
-
-    player1 = tk.Frame(score)
-    p1entry = tk.Entry(player1, width=10)
-    p1label = tk.Label(player1, text="Player 1")
-
-    player2 = tk.Frame(score)
-    p2entry = tk.Entry(player2, width=10)
-    p2label = tk.Label(player2, text="Player 2")
-
-    player3 = tk.Frame(score)
-    p3entry = tk.Entry(player3, width=10)
-    p3label = tk.Label(player3, text="Player 3")
-
-    player4 = tk.Frame(score)
-    p4entry = tk.Entry(player4, width=10)
-    p4label = tk.Label(player4, text="Player 4")
-
-    instruct = tk.Label(score, text = "Enter your names")
-    contin = tk.Button(score, text = "Continue", command = Start, bg = "green")
-
+    score = Scorekeeper()
     score.mainloop()
